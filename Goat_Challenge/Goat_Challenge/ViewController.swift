fe705a029d3762e4641c98426167c9a14d8f88be
wangtzuh <tzuhsuan@woobo.io>
2022-03-08 17:23:26 +0800

    Removed Main.StoryBoard
    Replaced with ViewController as rootViewController.
    Todo:
    - Update rootViewController as UINavigationController

--- Goat_Challenge/Goat_Challenge/ViewController.swift
+++ Goat_Challenge/Goat_Challenge/ViewController.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    private lazy var helloTextView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .gray
+        textView.text = "Hello Goat"
+        return textView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        view.backgroundColor = .white
+        view.addSubview(helloTextView)
+        helloTextView.snp.makeConstraints { make in
+            make.size.equalTo(100)
+            make.center.equalToSuperview()
+        }
     }
 
 
